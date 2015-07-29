306b59abd3612ea267352b851f3bbdfa615640cc
Andrew Clark <ac8774@gmail.com>
2015-07-29 00:29:17 -0400

    Update eel.js

--- eel.js
+++ eel.js
@@ -1,27 +1,13 @@
 (function(ext) {
-    // Cleanup function when the extension is unloaded
     ext._shutdown = function() {};
-
-    // Status reporting code
-    // Use this to report missing hardware, plugin or unsupported browser
-    ext._getStatus = function() {
-        return {status: 2, msg: 'Ready'};
-    };
-        ext.url = function(url) {
-            ScratchExtensions.loadExternalJS(url);
-    };
-        ext.url = function(text) {
-            eval(text);
-    };
-
-    // Block and block menu descriptions
+    ext._getStatus = function() {return {status: 2, msg: 'Ready'};};
+    ext.url = function(url) {ScratchExtensions.loadExternalJS(url);};
+    ext.url = function(text) {eval(text);};
     var descriptor = {
         blocks: [
         [' ', 'load from URL: %s', 'url'],
         [' ', 'load from text: %s', 'text'],
         ]
     };
-
-    // Register the extension
     ScratchExtensions.register('Easy Extension Loader', descriptor, ext);
 })({});
