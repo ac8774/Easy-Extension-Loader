d4e769b0ee60a0d39423f1b84233b74fc1866dea
Andrew Clark <ac8774@gmail.com>
2015-07-29 09:44:30 -0400

    Fixed all of my dumb mistakes...

--- eel.js
+++ eel.js
@@ -1,20 +1,20 @@
 (function(ext) {
     ext._shutdown = function() {};
     ext._getStatus = function() {return {status: 2, msg: 'Ready'};};
-    ext.load = function(source, text) {
-        if source = "URL" {ScratchExtensions.loadExternalJS(text);}
+    ext.load = function(text, source) {
+        if (source === "URL") {ScratchExtensions.loadExternalJS(text);}
         else
         {eval(text);}
-    }
-    ext.check = function(){return true}
+    };
+    ext.check = function(){return true;};
         var descriptor = {
         blocks: [
-        [' ', 'load from %m.source: %s', 'load'],
+        [' ', 'load %s from %m.source', 'load'],
         ['b', 'installed?', 'check']
-        ]
+        ],
         menus: {
         source: ['URL', 'text']
-    },
+    }
     };
     ScratchExtensions.register('Easy Extension Loader', descriptor, ext);
 })({});
