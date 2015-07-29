b9b3478e2866f3deb20ed4cfe0fa95d5c08f9dbd
Andrew Clark <ac8774@gmail.com>
2015-07-29 00:32:23 -0400

    Update eel.js

--- eel.js
+++ eel.js
@@ -2,7 +2,7 @@
     ext._shutdown = function() {};
     ext._getStatus = function() {return {status: 2, msg: 'Ready'};};
     ext.url = function(url) {ScratchExtensions.loadExternalJS(url);};
-    ext.url = function(text) {eval(text);};
+    ext.text = function(text) {eval(text);};
     var descriptor = {
         blocks: [
         [' ', 'load from URL: %s', 'url'],
