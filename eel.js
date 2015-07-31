098adf55fc5fa0fe409eb325e0600fe5cbb97862
Andrew Clark <ac8774@gmail.com>
2015-07-30 20:28:59 -0400

    even smaller fix

--- eel.js
+++ eel.js
@@ -7,12 +7,11 @@
         {eval(text);}
     };
     ext.check = function(){return true;};
-    ext.runall = function(){return true;};
         var descriptor = {
         blocks: [
         [' ', 'load %s from %m.source', 'load', 'https://rawgit.com/Iwotastic/Scratch-Boost/master/src/installer.js', 'URL'],
-        ['b', 'eel installed?', 'check']
-        ['h', 'load extensions here:', 'runall']
+        ['b', 'eel installed?', 'check'],
+        ['h', 'load extensions here:', 'check']
         ],
         menus: {
         source: ['URL', 'text']
